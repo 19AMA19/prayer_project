cc281c8bc02c8c94e1bf2647df4faaff0139ba52
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2023-03-13 16:28:41 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,175 +1,104 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
- import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
- import { getFirestore, collection, getDoc, getDocs, doc, query, addDoc} from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js'
+let selectOption = document.getElementById("selected-city")
+let city_title = document.getElementById("city_name")
+let bgImage = document.querySelector("#bgImage")
+let content = document.getElementById("content")
 
-
- // Your web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "AIzaSyCy3dYQI67hwhjJ2-R_JlhIhkAcQl29gbw",
-   authDomain: "isoapplication-c76e3.firebaseapp.com",
-   projectId: "isoapplication-c76e3",
-   storageBucket: "isoapplication-c76e3.appspot.com",
-   messagingSenderId: "688031718290",
-   appId: "1:688031718290:web:bdb590d1d76ac5ebc4bf4b"
- };
-
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const auth = getAuth();
- const db = getFirestore();
- const usrCol = collection(db, 'userData');
- const snapshots = await getDocs(usrCol);
-
-//  console.log(snapshots);
-
-
- var email = document.querySelector("#email")
- var password = document.querySelector("#password")
- const hideTable = document.querySelector('.table');
- const showUsers = document.querySelector('.ta');
- const showMessage = document.querySelector('.Ma');
-
- 
-
-// Check if user login
-auth.onAuthStateChanged(user => {
-  if (user) {
-    // Get users data from firestore
-    let html1 = ''; var index1 = 0; // For Drivers Page
-    let html2 = ''; var index2 = 0; // For Messages Page
-
-    getDocs(collection(db, "userData")).then(snapshot => {
-      snapshot.forEach((doc1) => {
-        const getUsers = doc1.data();
-        const li1 = `
-        <tr>
-            <td>${index1 +=1 }</td>
-            <td>${getUsers.fullname}</td>
-            <td>${getUsers.email}</td>
-            <td>${getUsers.department}</td>
-            <td>${getUsers.division}</td>
-            <td>${getUsers.role}</td>
-            <td>Active</td>
-        <tr>
-          `;
-        html1 += li1
-        showUsers.innerHTML = html1
-      });
-    });
-
-  getDocs(collection(db, "Messages")).then(snap => {
-    snap.forEach((doc2) => {
-      const UserMessages = doc2.data();
-      const li2 = `
-      <tr>
-          <td>${index2 +=1 }</td>
-          <td>${UserMessages.FULLNAME}</td>
-          <td>${UserMessages.EMAIL}</td>
-          <td>${UserMessages.MESSAGE}</td>
-          <td><button class="btn btn-success">Answer</button></td>
-
-      <tr>
-        `;
-      html2 += li2
-      showMessage.innerHTML = html2
-    });
-  });
-
-  } else {
-    // showMessage.innerHTML = `<h5 class="text-center justify-content-center"> Login to view data</h5>`;
-    // hideTable.style.display = "none";
-  }
-});
-
-//  Login Function
-
- window.login = function(e){
-e.preventDefault();
-    var user = {
-    email:email.value,
-    password:password.value,            
+function getPrayersTimings(cityName){
+    let params = {
+        country:"sa",
+        city :cityName
     }
-    signInWithEmailAndPassword(auth, user.email, user.password).then(function(success){
-        localStorage.setItem('user', JSON.stringify(user));
-        window.location.replace('./admin/index.html');
-        // alert("Login successfully");
+    
+    axios.get('http://api.aladhan.com/v1/timingsByCity', {
+        params: params
+        })
+        .then(function (response) {
+        let prayTimes = response.data.data.timings
+        let dateGregorian = response.data.data.date.gregorian.date
+        let dateHijri   = response.data.data.date.hijri.date
+
+        let weekday = response.data.data.date.hijri.weekday.ar
+        document.getElementById("Fajr").innerHTML = prayTimes.Fajr
+        // document.getElementById("Sunrise").innerHTML = prayTimes.Sunrise
+        document.getElementById("Dhuhr").innerHTML = prayTimes.Dhuhr
+        document.getElementById("Asr").innerHTML = prayTimes.Asr
+        document.getElementById("Maghrib").innerHTML = prayTimes.Maghrib
+        document.getElementById("Isha").innerHTML = prayTimes.Isha
+        document.getElementById("weekday").innerHTML = `${weekday}`
+        document.getElementById("gregorian_date").innerHTML = `${dateGregorian}`
+        document.getElementById("hijri_date").innerHTML = `${dateHijri}`
+        })
+        .catch(function (error) {
+        console.log(error);
+        },
+    )
+}
+
+
+    let cities = [
+        {
+            arabicName: "المدينة المنورة",
+            name: "medina",
+            imageUrl: "https://hotelsareamazing.com/wp-content/uploads/2020/08/Best-Hotels-in-Medina-Saudi-Arabia.jpg",
+            about: {
+                title: "وجهة تاريخية مهيبة",
+                content: "المدينة المنورة هي أول عاصمة في تاريخ الإسلام، وثاني أقدس الأماكن لدى المسلمين بعد مكة المكرمة، ما يجعلها إحدى الوجهات الرئيسية لملايين المسلمين من مختلف أنحاء العالم. تتمركز المدينة حول المسجد النبوي، الذي شيده الرسول الكريم بنفسه، وأيضًا المكان الذي دفن فيه. المدينة المنورة هي المكان الذي عاش فيه النبي محمد (ص) وأسس فيها أول دولة في الإسلام بعد هجرته من مكة المكرمة سنة 622 ميلادية. وتلك السنة على قدر كبير من الأهمية في تاريخ الإسلام، حيث تمثل بدء التقويم الهجري. وكانت المدينة المدينة المنورة تسمى بيثرب، ثم تم تغير اسمها إلى المدينة المنورة إثر دخول النبي محمد (ص) إليها، وأصبحت زيارة المدينة بمثابة حلم حياة الكثير من المسلمين."
+            } 
+        },
+        {
+            arabicName: "تبوك",
+            name: "Tabūk",
+            imageUrl: "./images/pic3.jpeg",
+            about: {
+                title: "أراضٍ خصبة وكنوز أثرية",
+                content: "لطالما كانت مدينة تبوك، عاصمة منطقة تبوك في شمال غرب السعودية، نقطة استراحة للحجاج الأردنيين والمصريين. وتظهر ثقافتها الغنية جليًا في سوق الطواحين، الذي يشتهر ببيع السجاد المزركش وأغطية الخيم المصنوعة من جلد الماعز. وعادة ما تتدفق جموع الزوار لزيارة المواقع الأثرية القديمة والموقع المذكور في قصة النبي موسى الذي سكن شرق المدينة لمدة عقد من الزمن. تمثّل المدن الشاطئية الساحرة في تبوك، مثل حقل وشرما، بداية الساحل السعودي، وتتميز بمياه شفافة وشواطئ نقية. كما تتوفر المزيد من التضاريس الطبيعية التي يمكنك استكشافها: مثل المقابر المنحوتة في مغاير شعيب، أو عيون موسى بالقرب من مقنا حيث تتدفّق الينابيع الطبيعية أسفل أشجار النخيل، أو منطقة طيب الاسم المذهلة، وهي عبارة عن وادٍ من صخور الغرانيت المنحدرة يفصل بينه وبين خليج العقبة الفيروزي الطريق فقط."
+            } 
+        },
+        {
+            arabicName: "حائل",
+            name: "Ḩā'il",
+            imageUrl: "./images/pic1.jpeg",
+            about: {
+                title: "زاخرة بالتاريخ ويكتنفها الغموض",
+                content: " تقع حائل بين جبل شمر شمالًا وجبل سلمى جنوبًا. وكانت في يوم ما عاصمة كافة الصحراء العربية وموطنًا لأساطير مثل حاتم الطائي، الشاعر العربي الذي ذاع صيته من خلال قصص مثل (التي تُعرف أيضًا باسم . أما اليوم، فهي عاصمة المنطقة الشمالية الوسطى والتي تحمل الاسم نفسه، وتعد محطة هامة للحجاج المتجهين إلى مكة المكرمة. وتشتهر مدينة حائل أيضًا باستضافة الفعاليات العالمية، ومنها مهرجان الصحراء الذي يحتفي بثقافة المنطقة، ورالي حائل الدولي، حيث تقام سباقات رالي السيارات وعربات الدفع الرباعي والدرجات النارية عبر صحراء النفوذ وحائل والبقاع والغزالة وقرية أم القلبان. فإذا كنت مولعًا بالتاريخ والثقافة والمغامرة، وجه نظرك إلى حائل."
+            } 
+        },
+        {
+            arabicName: "جازان",
+            name: "Jāzān",
+            imageUrl: "./images/pic4.jpeg",
+            about: {
+                title: "عن المدينة",
+                content: "تقع منطقة جازان في الجزء الجنوبي الغربي من المملكة العربية السعودية على ساحل البحر الأحمر، بين خطي عرض 16 درجة والطول 42 درجة، وتقدر مساحتها بحوالي 35 ألف كيلو متر مربع ، وتعتبر هذه المنطقة همزة وصل بين التجارة البرية والبحرية للمنطقة الجنوبية إذ ان بها ميناء جازان ثالث موانئ المملكة من حيث السعة وتعتبر البوابة الرئيسية لواردات الجزء الجنوبي الغربي من المملكة وهي محطة استراحة للحجيج القادمين من اليمن بحكم موقعها على الطريق الذي يربط بين اليمن ومكة المكرمة، وتضم سد وادي جازان من اكبر سدود المملكة إذ تبلغ طاقته التخزينية 7 ملايين متر مكعب وهي غنية بالإنتاج الزراعي، كما تضم معالم تاريخية قديمة ."
+            } 
+        }
+
+        
+    ]
+
+    for(city of cities){
+        const content = `
+        <option>${city.arabicName}</option>
+        `
+        selectOption.innerHTML += content
+    }
+
+
+
+
+    selectOption.addEventListener("change", function(){
+        city_title.innerHTML = this.value        
+        let cityName = ""
+  
+
+        for(let city of cities){
+            if(city.arabicName == this.value){
+                cityName = city.name
+                document.getElementById("title").innerHTML = city.about.title
+                document.getElementById("content").innerHTML = city.about.content
+         
+                bgImage.style.background = 'url('+ city.imageUrl +')'
+            }
+            getPrayersTimings(cityName)
+        }
     })
-    .catch(function(error){
-        alert("Error "+ error)
-    })
-}
-
-// Logout Function
-window.logout = function(e){
-    e.preventDefault();
-    signOut(auth).then(() => {
-        alert("Logout successfully ...");
-        sessionStorage.removeItem('user');
-        localStorage.removeItem('user');
-
-
-        console.log("Logout");
-
-        window.location.replace('../index.html');
-      }).catch((error) => {
-        alert("Error: " + error);
-    });
-}
-
-
-// ========================================= User Profile ========================================
-const myName = document.querySelector('#MyName');
-const myEmail = document.querySelector('#MyEmail');
-const myRole = document.querySelector('#MyRole');
-const myPic = document.querySelector('#MyProfilePic');
-const myDiv = document.querySelector('#MyDivision');
-const myDept = document.querySelector('#MyDepartment');
-const lastLogin = document.querySelector('#LastLogin');
-
-
-
-const user = auth.currentUser;
-
-if (user) {
-  getDocs(collection(db, "userData")).then(snapshot => {
-    snapshot.forEach((doc) => {
-      const getUserInformation = doc.data();
-      if(getUserInformation.uid == user.uid){
-        myPic.innerHTML   = `<img id="MyProPic" src="${getUserInformation.imageUrl}" alt=""></img>`
-        myName.innerHTML  = `<h3 class="text-center justify-content-center">${getUserInformation.fullname}</h3>`;
-        myEmail.innerHTML = `<h3 class="text-center justify-content-center">${getUserInformation.email}</h3>`;
-        myDiv.innerHTML  = `<h3 class="text-center justify-content-center">${getUserInformation.division}</h3>`;
-        myDept.innerHTML  = `<h3 class="text-center justify-content-center">${getUserInformation.department}</h3>`;
-        myRole.innerHTML  = `<h3 class="text-center justify-content-center">Account Type: (${getUserInformation.role})</h3>`;
-        lastLogin.innerHTML  = `<h4 class="text-center justify-content-center text-warning">Last Login: ${user['metadata'].lastSignInTime}</h4>`;
-      }
-    });
-  });
-
-//  console.log(user);
-} else {
-  // alert('Please login')
-}
-
-
-// ========================================= Contact us ============================================
-
-// Add Message
-window.addMessage = function(e){
-  e.preventDefault();
-
-  let CNAME = document.querySelector('#CName').value;
-  let CEMAIL = document.querySelector('#CEmail').value;
-  let CMESSAGE = document.querySelector('#CMessage').value;
-  let Alert = document.querySelector('#ALERT');
-
-  addDoc(collection(db, "Messages"),{
-    FULLNAME : CNAME,
-    EMAIL : CEMAIL,
-    MESSAGE : CMESSAGE,
-  });
-    Alert.style.display = "block";
-    document.getElementById("myForm").reset();
-
-}
